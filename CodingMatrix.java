245d448a05e59a264b50b4c810ace52e2c9cf696
Artur <artur.balczynski@student.put.poznan.pl>
2019-10-22 00:44:23 +0200

    calculate matrix parameters

--- CodingMatrix.java
+++ CodingMatrix.java
@@ -4,32 +4,90 @@ import java.util.Arrays;
 public class CodingMatrix {
     private final String key; 
     private final char[] key_array; 
-    private final int key_length; 
+
+    private final int width; 
     private final int hight; 
-    private final int index_sum; 
-    private final ArrayList<Pair<Character, Integer>> characters_indexes; 
+    private final int index_sum;
+
+    private int[] key_index; 
 
     private ArrayList<ArrayList<Character>> matrix; 
 
     public CodingMatrix(String key) {
         this.key = key; 
-        key_length = key.length(); 
+        this.key_array = key.toCharArray(); 
+        width = key.length(); 
 
         int sum=0; 
-        for(int i=1; i<=key_length; i++) {
+        for(int i=1; i<=width; i++) {
             sum += i; 
         }
 
         index_sum = sum; 
-        hight = index_sum/key_length; 
+        hight = index_sum/width; 
 
         matrix = new ArrayList<>(hight); 
 
         for(int i=1; i<=hight; i++) {
-            matrix.add(new ArrayList<>(key_length));  
+            matrix.add(new ArrayList<>(width));  
         }
 
-        key_array = sortKeyCharacters(); 
+        char[] temp = key_array.clone(); 
+        key_index = findKeyIndex(temp); 
+    }
+
+    private int findIndexOfMin(char[] charArray, int range) {
+        char min = Character.MAX_VALUE; 
+        int result = -1; 
+
+        for (int i = 0; i < range; i++) {
+            System.out.println(min);
+            System.out.println(charArray[i]);
+
+            if(Character.compare(charArray[i], min) < 0) {
+                min=charArray[i]; 
+                result=i; 
+            }
+        }
+
+        return result; 
+    }
+
+        private int findIndexOfMin(char[] charArray) {
+        char min = Character.MAX_VALUE; 
+        int result = -1; 
+
+        for (int i = 0; i < charArray.length; i++) {
+            System.out.println(min);
+            System.out.println(charArray[i]);
+
+            if(Character.compare(charArray[i], min) < 0) {
+                min=charArray[i]; 
+                result=i; 
+            }
+        }
+
+        return result; 
+    }
+
+    private void swap(char[] charArray, int x, int y) {
+        char temp=charArray[x]; 
+        charArray[x]=charArray[y]; 
+        charArray[y]=temp; 
+    }
+
+    private int[] findKeyIndex(char[] charArray) {
+        int[] result = new int[charArray.length]; 
+        int currentIndex = 1; 
+
+        for (int i = charArray.length; i >= 1; i--) {
+            int minIndex = findIndexOfMin(charArray); 
+            result[minIndex] = currentIndex; 
+            charArray[minIndex] = Character.MAX_VALUE; 
+            currentIndex++; 
+        }
+
+        return result; 
     }
 
     private char[] sortKeyCharacters() {
@@ -41,11 +99,16 @@ public class CodingMatrix {
     public void debug() {
         System.out.println("Debug");
         System.out.println(key);
-        System.out.println(key_length);
+        System.out.println(width);
         System.out.println(hight);
         System.out.println(index_sum);
 
         for (char e : key_array) {
+            System.out.println(e);            
+        }
+
+        
+        for (int e : key_index) {
             System.out.println(e);            
         }
     }
